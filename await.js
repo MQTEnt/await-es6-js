ca9919f3bd9ab71077dc8200b1a8218100a75ecc
mqtent <mqtent@gmail.com>
2019-05-07 13:15:24 +0700

    Update await example

--- await.js
+++ await.js
@@ -1,18 +1,24 @@
+var axios = require('axios');
+
 execute()
 
 function execute() {
-  getData();
+  getData().then(result => {
+  	// console.log(result)}
+  }); // ===> Promise
   console.log('End');
 }
 
 async function getData() {
   console.log('Before get data');
 
-  var aw = await setTimeout(function() {
-    console.log('Get data');
-  }, 3000);
+  var result = await axios.get('http://dummy.restapiexample.com/api/v1/employees');
+
+  console.log(result);
 
   console.log('After get data');
+
+  return result;
 }
 
 
@@ -20,8 +26,8 @@ async function getData() {
 /*
 Before get data
 End
+... data ...
 After get data
-Get data
 */
 
 // Await will convert code after it into code in callback, so \console.log('After get data');\ is excuted in callback of aw variable. That's why 'After get data' display after 'End'
